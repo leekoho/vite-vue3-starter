97fee25662d9f6857863c48ef892e7d182b48d25
李嘉豪 <leekoho95@163.com>
2022-06-16 20:34:34 +0800

    add plugin vite-plugin-html-env

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,17 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import Pages from 'vite-plugin-pages'
 import Layouts from 'vite-plugin-vue-layouts'
+import HtmlEnv from 'vite-plugin-html-env'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), Pages(), Layouts()],
+  plugins: [
+    vue(),
+    // see https://github.com/hannoeru/vite-plugin-pages
+    Pages(),
+    // see https://github.com/JohnCampionJr/vite-plugin-vue-layouts
+    Layouts(),
+    // see https://github.com/lxs24sxl/vite-plugin-html-env
+    HtmlEnv(),
+  ],
 })
