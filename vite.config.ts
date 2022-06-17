6e4dec20a0970d44d3d1e0a65d4485c001fcea13
李嘉豪 <leekoho95@163.com>
2022-06-17 14:06:12 +0800

    add alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import Pages from 'vite-plugin-pages'
@@ -7,6 +8,11 @@ import WindiCSS from 'vite-plugin-windicss'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src'),
+    },
+  },
   plugins: [
     vue(),
     // see https://github.com/hannoeru/vite-plugin-pages
